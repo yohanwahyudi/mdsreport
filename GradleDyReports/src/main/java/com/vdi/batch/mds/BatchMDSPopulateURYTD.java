a057c1cdb968dd799a000cc7bfb7e88c20b1e660
ywahyudi <yohan.wahyudi@visionet.co.id>
2019-03-15 15:36:16 +0700

    commit

---
+++ GradleDyReports/src/main/java/com/vdi/batch/mds/BatchMDSPopulateURYTD.java
@@ -0,0 +1,45 @@
+package com.vdi.batch.mds;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import com.vdi.batch.mds.helper.PopulateUserRequestYTD;
+import com.vdi.configuration.AppConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class BatchMDSPopulateURYTD extends QuartzJobBean {
+
+	Logger logger = LogManager.getLogger(BatchMDSPopulateURYTD.class);
+
+	private AnnotationConfigApplicationContext ctx;
+
+	@Override
+	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+
+		logger.info("Execute BatchMDSPopulateURYTD......");
+
+		ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+		PopulateUserRequestYTD populateUR = ctx.getBean(PopulateUserRequestYTD.class);
+		
+		populateUR.populate();
+		
+		HikariDataSource hds = ctx.getBean("dataSource", HikariDataSource.class);
+		logger.info("close datasource");
+		logger.info(hds.getPoolName()+"-"+hds.getJdbcUrl());
+		try {
+			hds.close();
+		} catch (Exception e) {
+			logger.info("Error closing datasource ");
+			e.printStackTrace();
+		}
+		
+		logger.info("Execute BatchMDSPopulateURYTD finished......");
+
+
+	}
+
+}
